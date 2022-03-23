4468668657b221471bbdf0519b6b63de653c50a2
Vansh <vanshwaldeo360@gmail.com>
2022-03-23 16:50:35 +0530

    added divide feature

---
+++ divide.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    cout << "This Program Will Divide two numbers given by the user"<<endl ;
+    cout << "Enter first number:  " <<endl;
+    float a;
+    cin >> a ;
+    cout << "Enter second number" <<endl;
+    float b;
+    cin >> b;
+    float c = a/b;
+    cout << "The result of division is: " << c << endl;
+}
