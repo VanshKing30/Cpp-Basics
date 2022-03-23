ac070d76e73e58fff7701abd0343882fef895e4a
Vansh <vanshwaldeo360@gmail.com>
2022-03-23 16:31:45 +0530

    Added Multiply and run.exe

---
+++ multiply.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    cout<< "This Program Will Multiply two digits you want to."<<endl;
+    cout << " Enter The First digit "<<endl;
+    int a;
+    cin >> a ;
+    cout << "Enter second digit" <<endl ; 
+    int b;
+    cin >> b; 
+    int c = a*b ;
+    cout << "The multiplication of both the umbers is: " << c <<endl; 
+}
