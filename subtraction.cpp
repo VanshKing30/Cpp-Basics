1370f12165924e5e0c87d65b2da3848e4b9a6d7b
Vansh <vanshwaldeo360@gmail.com>
2022-03-23 16:24:49 +0530

    Subtraction added

---
+++ subtraction.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<< "Program To Perform subtraction between two numbers given by the user." <<endl ;
+    cout << "Enter The first digit: " <<endl ;
+    int a;
+    cin >> a ; 
+    cout << "Enter The Second digit:  " <<endl;
+    int b;
+    cin >> b;
+    int c = a - b ;
+    cout << "The subtraction of Two numbers which i recieved is: " << c << endl ;
+
+}
+
